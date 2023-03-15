82a274b562eae09498b2fd4d286872b6e61a46b5
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2023-03-15 21:13:10 +0100

    Shuffled and styled sponsors list

--- components/sponsors/Sponsors.tsx
+++ components/sponsors/Sponsors.tsx
@@ -12,22 +12,25 @@ export default function Sponsors({ sponsors }: Props): JSX.Element | null {
     <div className="border-t border-svw-blue-default">
       <div className="container">
         <h2 className="flex flex-row justify-center">Unsere Partner</h2>
-        <div className="p-3 flex flex-row flex-wrap place-content-between">
-          {sponsors?.map((sponsor) => {
-            const imageAttributes = sponsor.attributes?.image?.data?.attributes;
-            if (imageAttributes) {
-              return (
-                <Image
-                  key={sponsor.id}
-                  src={getFullAssetUrl(imageAttributes?.url)}
-                  alt={imageAttributes.name}
-                  width={imageAttributes.width ?? 100}
-                  height={imageAttributes.height ?? 100}
-                  className="object-scale-down max-h-32"
-                />
-              );
-            }
-          })}
+        <div className="p-3 flex flex-col items-center md:grid md:grid-cols-2 md:gap-2 lg:grid-cols-3 xl:grid-cols-4">
+          {sponsors
+            ?.sort(() => 0.5 - Math.random()) // shuffle array
+            .map((sponsor) => {
+              const imageAttributes =
+                sponsor.attributes?.image?.data?.attributes;
+              if (imageAttributes) {
+                return (
+                  <Image
+                    key={sponsor.id}
+                    src={getFullAssetUrl(imageAttributes?.url)}
+                    alt={imageAttributes.name}
+                    width={imageAttributes.width ?? 100}
+                    height={imageAttributes.height ?? 100}
+                    className="object-scale-down md:h-32 w-52 md:w-full"
+                  />
+                );
+              }
+            })}
         </div>
       </div>
     </div>
