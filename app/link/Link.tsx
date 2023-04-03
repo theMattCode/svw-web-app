181a305c3562aa1f8eed7bd3ab5a944a4648f9f4
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2023-04-03 16:40:26 +0200

    Support links to sponsor websites
    theMattCode/svw-web-app#19

---
+++ app/link/Link.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { ComponentProps, PropsWithChildren } from "react";
+import { UrlObject } from "url";
+
+type Props = Omit<PropsWithChildren<ComponentProps<typeof Link>>, "href"> & {
+  href: string | UrlObject | undefined | null;
+};
+
+export function OptionalLink({ href, children, ...props }: Props): JSX.Element {
+  if (href) {
+    return (
+      <Link href={href} {...props}>
+        {children}
+      </Link>
+    );
+  }
+
+  return <>{children}</>;
+}
--- components/sponsors/Sponsors.tsx
+++ components/sponsors/Sponsors.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import { getFullAssetUrl } from "#/lib/asset";
 import { SponsorEntity } from "#/lib/graphql/generated";
+import { OptionalLink } from "#/app/link/Link";
 
 type Props = {
   sponsors?: SponsorEntity[] | null;
 };
+
+function shuffle(): number {
+  return 0.5 - Math.random();
+}
 
 export default function Sponsors({ sponsors }: Props): JSX.Element | null {
   if (!sponsors) return null;
@@ -13,24 +18,26 @@ export default function Sponsors({ sponsors }: Props): JSX.Element | null {
       <div className="container">
         <h2 className="flex flex-row justify-center">Unsere Partner</h2>
         <div className="p-3 flex flex-col items-center md:grid md:grid-cols-2 md:gap-2 lg:grid-cols-3 xl:grid-cols-4">
-          {sponsors
-            ?.sort(() => 0.5 - Math.random()) // shuffle array
-            .map((sponsor) => {
-              const imageAttributes =
-                sponsor.attributes?.image?.data?.attributes;
-              if (imageAttributes) {
-                return (
+          {sponsors?.sort(shuffle).map((sponsor) => {
+            const imageAttributes = sponsor.attributes?.image?.data?.attributes;
+            if (imageAttributes) {
+              return (
+                <OptionalLink
+                  key={sponsor.id}
+                  href={sponsor.attributes?.url}
+                  target="_blank"
+                >
                   <Image
-                    key={sponsor.id}
                     src={getFullAssetUrl(imageAttributes?.url)}
                     alt={imageAttributes.name}
                     width={imageAttributes.width ?? 100}
                     height={imageAttributes.height ?? 100}
                     className="object-scale-down md:h-32 w-52 md:w-full"
                   />
-                );
-              }
-            })}
+                </OptionalLink>
+              );
+            }
+          })}
         </div>
       </div>
     </div>
