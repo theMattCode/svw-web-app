7d937f34b3a1d9ee486e2b9e7f7eedaa48a12739
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2023-04-12 08:54:25 +0200

    Tweak sponsor styling

--- components/sponsor/SponsorGrid.tsx
+++ components/sponsor/SponsorGrid.tsx
@@ -15,7 +15,7 @@ export default function SponsorGrid({ sponsors }: Props): JSX.Element | null {
     <div className="bg-white border-t border-svw-blue-default py-4">
       <div className="container">
         <h2 className="flex flex-row justify-center">Unsere Partner</h2>
-        <div className="p-3 flex flex-col items-center md:grid md:grid-cols-2 gap-4 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="p-3 flex flex-col items-center sm:grid sm:grid-cols-2 gap-4 lg:grid-cols-3 xl:grid-cols-4">
           {sponsors?.sort(shuffle).map((sponsor) => (
             <Sponsor key={sponsor.id} sponsor={sponsor} />
           ))}
