56c69934de1df1eed87f37b51746239bc242c153
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2023-04-14 07:17:48 +0200

    Style position and dimensions of Fussball.de widgets
    themattcode/svw-web-app#44

--- components/widget/Fussball.de.tsx
+++ components/widget/Fussball.de.tsx
@@ -4,18 +4,16 @@ type Props = {
 
 export function FussballDeWidget({ widgetKey }: Props): JSX.Element {
   return (
-    <div className="justify-self-end self-end items-end place-self-end">
-      <iframe
-        width={450}
-        height={450}
-        src={
-          "//www.fussball.de/widget2/-/schluessel/" +
-          widgetKey +
-          "/target/" +
-          widgetKey +
-          "/caller/www.svwalddorf.de"
-        }
-      />
-    </div>
+    <iframe
+      className="self-center w-full max-w-[900px]"
+      height={500}
+      src={
+        "//www.fussball.de/widget2/-/schluessel/" +
+        widgetKey +
+        "/target/" +
+        widgetKey +
+        "/caller/www.svwalddorf.de"
+      }
+    />
   );
 }
