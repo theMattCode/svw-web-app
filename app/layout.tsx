d52c56bb3e712dc978ee696e29d52fcf3a991c3e
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2023-10-11 12:04:50 +0200

    Shuffle sponsors

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default async function Layout({
         <Navigation />
 
         <main className="">{children}</main>
-        <SponsorGrid />
+        <SponsorGrid shuffle={true} />
         <Footer />
       </body>
     </html>
