a61e84bb7af3b0e52c0808a326b04b5a3896010f
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2023-10-11 07:15:11 +0200

    Cleanup data fetching and old nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,15 @@
 import "#/styles/globals.css";
 import React, { PropsWithChildren } from "react";
 import Footer from "#/components/footer/Footer";
-import { fetchBasicData, fetchMeta } from "#/app/data.gql";
 import SponsorGrid from "#/components/sponsor/SponsorGrid";
 import { Metadata } from "next";
 import { Navigation } from "#/components/navigation/Navigation";
+import { club } from "#/content/club";
 
 export async function generateMetadata(): Promise<Metadata> {
-  const data = await fetchMeta();
-  const meta = data?.meta?.data?.attributes;
   return {
-    title: meta?.title ?? "",
-    description: meta?.description ?? "",
+    title: club.short,
+    description: "Webseite von SV Walddorf 1904 e.V.",
     viewport: "width=device-width, initial-scale=1",
     icons: {
       icon: "/favicon.ico",
@@ -23,8 +21,6 @@ export async function generateMetadata(): Promise<Metadata> {
 export default async function Layout({
   children,
 }: PropsWithChildren): Promise<JSX.Element> {
-  const data = await fetchBasicData();
-  const footerData = data?.footer?.data?.attributes ?? null;
   return (
     <html lang="de">
       <head>
