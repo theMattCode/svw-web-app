ac24229a8a833538ba41bf250f6757052ad57823
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2023-10-26 11:27:12 +0200

    Generate article pages statically

--- app/aktuelles/page.tsx
+++ app/aktuelles/page.tsx
@@ -1,12 +1,18 @@
 import { PageBase } from "#/components/page/PageBase";
 import { ArticleList } from "#/components/articles/ArticleList";
 import { PageProps } from "#/lib/page";
+import { PaginatedArticleMatters } from "#/content/article";
+import { getURL } from "#/lib/url";
 
-export default function Aktuelles({ searchParams }: PageProps) {
+const PAGE_SIZE = 20;
+
+export default async function Aktuelles({ searchParams }: PageProps) {
   const page = searchParams.page ? Number.parseInt(searchParams.page) : 1;
+  const articlesResponse = await fetch(`${getURL()}/api/articles?page=${page}&pageSize=${PAGE_SIZE}`);
+  const paginatedArticles: PaginatedArticleMatters = await articlesResponse.json();
   return (
     <PageBase>
-      <ArticleList pageSize={20} page={page} />
+      <ArticleList paginatedArticleMatters={paginatedArticles} />
     </PageBase>
   );
 }
--- app/article/[slug]/page.tsx
+++ app/article/[slug]/page.tsx
@@ -2,12 +2,18 @@ import Article from "#/components/articles/Article";
 import { PageBase } from "#/components/page/PageBase";
 import { PageProps } from "#/lib/page";
 import { getArticle } from "#/content/article";
+import fs from "fs";
 
 export default function Page({ params }: PageProps) {
-  const article = getArticle(params.slug);
+  const article = getArticle(params.slug, "public/content/article");
   return (
     <PageBase>
       <Article article={article} />
     </PageBase>
   );
+}
+
+export function generateStaticParams() {
+  const allFiles = fs.readdirSync("public/content/article").reverse();
+  return allFiles.map((filename) => ({ slug: filename.replace(".md", "") }));
 }
