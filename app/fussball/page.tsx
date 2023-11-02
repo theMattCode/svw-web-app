2f1409f0564d5a0a236f1d1a2b6480bfa4d11067
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2023-11-02 19:16:55 +0100

    Flip matches widget and articles

--- app/fussball/page.tsx
+++ app/fussball/page.tsx
@@ -7,18 +7,15 @@ import { ArticlePreviewList } from "#/components/articles/ArticlePreviewList";
 
 export default function Fussball() {
   return (
-    <>
-      <div className="px-2 pb-2 flex flex-col bg-neutral-200 gap-2">
-        <div className="py-4 w-full bg-svw-blue-default text-white">
-          <SubMenu item={fussball} />
-        </div>
-        <BlockTitle title="Fussball" />
-        <ArticlePreviewList tags={["Fussball"]} showTitle={false} />
-        <BlockTitle title="Spielplan" />
-        <div className="container bg-white shadow-2xl p-2 flex flex-col gap-2">
-          <FussballDeWidget widgetKey="02KJUU7I0O000000VUM1DNPBVTLF6IIO" height={1000} />
-        </div>
+    <div className="px-2 pb-2 flex flex-col bg-neutral-200 gap-2">
+      <div className="py-4 w-full bg-svw-blue-default text-white">
+        <SubMenu item={fussball} />
       </div>
-    </>
+      <BlockTitle title="Fussball" />
+      <div className="container bg-white shadow-2xl p-2 flex flex-col gap-2">
+        <FussballDeWidget widgetKey="02KJUU7I0O000000VUM1DNPBVTLF6IIO" height={1000} />
+      </div>
+      <ArticlePreviewList tags={["Fussball"]} />
+    </div>
   );
 }
