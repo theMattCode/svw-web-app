68cdf9fc40e9afcada07cf18c4e8f166b8845f14
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2024-03-30 15:39:16 +0100

    Fix base URL

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -4,9 +4,11 @@ export default initAuth0({
   secret: process.env.AUTH0_SECRET,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   baseURL:
-    process.env.AUTH0_BASE_URL ?? process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : "http://localhost:3000",
+    process.env.VERCEL_ENV === "production"
+      ? "https://svwalddorf.de"
+      : process.env.VERCEL_ENV === "preview"
+        ? `https://${process.env.VERCEL_URL}`
+        : "http://localhost:3000",
   clientID: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
 });
