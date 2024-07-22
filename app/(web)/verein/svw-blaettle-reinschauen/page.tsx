6a8fadaf7c173e25124db24ec66a3a5e8df13ac3
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2024-07-22 10:09:33 +0200

    Content Update

---
+++ app/(web)/verein/svw-blaettle-reinschauen/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BlockTitle } from "#/components/block-title/BlockTitle";
+import { PageBase } from "#/components/page/PageBase";
+import { PDFViewer } from "#/components/pdf-viewer/PDFViewer";
+import Image from "next/image";
+import { calcImageDimensionsForWidth } from "#/lib/image";
+import Link from "next/link";
+import { Metadata } from "next";
+import { getTitle } from "#/lib/page";
+import { svwBlaettle } from "#/content/sitemap";
+
+export const metadata: Metadata = {
+  title: getTitle(svwBlaettle.name),
+};
+
+export default function SVWBlaettleReinschauen() {
+  const imageDimensions = calcImageDimensionsForWidth({ width: 3497, height: 4961 }, 300);
+  return (
+    <PageBase>
+      <BlockTitle title={svwBlaettle.name} />
+      <div className="bg-white shadow-2xl p-2 flex flex-col lg:flex-row gap-2">
+        <PDFViewer url="/download/svw_blaettle_web_2022-2023.pdf" />
+      </div>
+    </PageBase>
+  );
+}
