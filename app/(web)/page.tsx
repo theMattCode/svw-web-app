08a00c1dceb812f8dfd033329ad7ef8b1e8d9a56
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2024-12-09 15:42:39 +0100

    Remove calendars

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -1,8 +1,6 @@
 import { Hero } from "#/components/hero/hero";
 import { AnnouncementsBlock } from "#/components/announcements/AnnouncementsBlock";
 import { ArticlePreviewList } from "#/components/articles/ArticlePreviewList";
-import { EventCalendar } from "#/components/calendar/Calendar";
-import { BlockTitle } from "#/components/block-title/BlockTitle";
 import { JSX } from "react";
 
 export default async function IndexPage(): Promise<JSX.Element | null> {
@@ -11,12 +9,6 @@ export default async function IndexPage(): Promise<JSX.Element | null> {
       <Hero />
       <AnnouncementsBlock />
       <div className="p-4 bg-neutral-200 flex flex-col gap-4">
-        <div className="container flex flex-col gap-2">
-          <BlockTitle title="Veranstaltungen und Termine" />
-          <div className="bg-white p-2 shadow-xl">
-            <EventCalendar agendaOnly={true} />
-          </div>
-        </div>
         <ArticlePreviewList pageSize={10} />
       </div>
     </div>
--- app/(web)/verein/veranstaltungen/page.tsx
+++ app/(web)/verein/veranstaltungen/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BlockTitle } from "#/components/block-title/BlockTitle";
 import { PageBase } from "#/components/page/PageBase";
-import { EventCalendar } from "#/components/calendar/Calendar";
 import { Metadata } from "next";
 import { getTitle } from "#/lib/page";
 import { veranstaltungen } from "#/content/sitemap";
@@ -14,9 +13,7 @@ export default function Veranstaltungen() {
   return (
     <PageBase>
       <BlockTitle title={veranstaltungen.name} />
-      <div className="bg-white shadow-2xl p-2 flex flex-col lg:flex-row gap-2">
-        <EventCalendar />
-      </div>
+      <div className="bg-white shadow-2xl p-2 flex flex-col lg:flex-row gap-2">To be replaced</div>
     </PageBase>
   );
 }
