c3a4821da9365e2b3d5e42f37d96269629fd49a0
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2024-12-12 11:20:33 +0100

    Upgrade to Storybook 8

--- components/match/SoccerMatch.stories.tsx
+++ components/match/SoccerMatch.stories.tsx
@@ -9,18 +9,40 @@ const meta: Meta<typeof SoccerMatchReport> = {
       type: "soccer",
       opponents: {
         home: { name: "SV Walddorf 1904 e.V.", logo: "/media/svw-emblem.svg" },
-        away: { name: "FC Another Team Which May Wrap", logo: "/media/logo/sv-walddorf-2023.png" },
+        away: {
+          name: "FC Another Team Which May Wrap",
+          logo: "/media/logo/sv-walddorf-2023.png",
+        },
       },
       competition: {
         name: "Competition",
-        link: { title: "Fussball.de", url: "https://fussball.de", extern: true },
+        link: {
+          title: "Fussball.de",
+          url: "https://fussball.de",
+          extern: true,
+        },
         season: "1. Bundesliga",
         round: "1. Spieltag",
       },
-      result: { home: { final: 2, halftime: 1 }, away: { final: 1, halftime: 0 } },
+      result: {
+        home: { final: 2, halftime: 1 },
+        away: { final: 1, halftime: 0 },
+      },
       events: [
-        { type: "goal", subtype: "regular", team: "home", time: 40, scorer: "Julian Lichtenberger" },
-        { type: "goal", subtype: "regular", team: "home", time: 60, scorer: "Paul Hellwig" },
+        {
+          type: "goal",
+          subtype: "regular",
+          team: "home",
+          time: 40,
+          scorer: "Julian Lichtenberger",
+        },
+        {
+          type: "goal",
+          subtype: "regular",
+          team: "home",
+          time: 60,
+          scorer: "Paul Hellwig",
+        },
       ],
     },
   },
