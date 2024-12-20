41633ec38ba1e8434d9e6fe08a762573ffd41f8d
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2024-12-20 07:59:57 +0100

    Design initial DB schema for people

---
+++ lib/db/schema/people.ts
@@ -0,0 +1,48 @@
+import { index, pgTable, text, uuid } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
+export const people = pgTable("people", {
+  id: uuid().primaryKey(),
+  firstName: text("first_name").notNull(),
+  lastName: text("last_name").notNull(),
+  email: text(),
+  phone: text(),
+  image: text(),
+});
+
+export const roles = pgTable("roles", {
+  id: uuid().primaryKey(),
+  name: text().notNull(),
+});
+
+export const peopleToRoles = pgTable(
+  "people_to_roles",
+  {
+    peopleId: uuid("people_id")
+      .notNull()
+      .references(() => people.id),
+    roleId: uuid("role_id")
+      .notNull()
+      .references(() => roles.id),
+  },
+  (t) => [index("pk").on(t.peopleId, t.roleId)],
+);
+
+export const peopleRelations = relations(people, ({ many }) => ({
+  peopleToRoles: many(peopleToRoles),
+}));
+
+export const rolesRelations = relations(roles, ({ many }) => ({
+  peopleToRoles: many(peopleToRoles),
+}));
+
+export const peopleToRolesRelations = relations(peopleToRoles, ({ one }) => ({
+  roles: one(roles, {
+    fields: [peopleToRoles.roleId],
+    references: [roles.id],
+  }),
+  people: one(people, {
+    fields: [peopleToRoles.peopleId],
+    references: [people.id],
+  }),
+}));
