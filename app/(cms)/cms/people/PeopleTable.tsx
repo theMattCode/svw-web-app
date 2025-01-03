cd951b1b512c0de8e885e7c3007bdd4bd7be1119
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2025-01-03 09:33:49 +0100

    Implement basic people list
    #226

---
+++ app/(cms)/cms/people/PeopleTable.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { PersonWithRoles } from "#/lib/types/people";
+import { createColumnHelper, getCoreRowModel, TableOptions } from "@tanstack/table-core";
+import { Table } from "#/components/cms/table/Table";
+import { PhoneCell } from "#/components/cms/table/cell/Phone";
+import { MailCell } from "#/components/cms/table/cell/Mail";
+import { RolesCell } from "#/components/cms/table/cell/Roles";
+
+const COLUMN_HELPER = createColumnHelper<PersonWithRoles>();
+
+export default function PeopleTable({ data }: { data: PersonWithRoles[] }) {
+  const options: TableOptions<PersonWithRoles> = {
+    data,
+    columns: [
+      COLUMN_HELPER.accessor("firstName", {
+        header: "Vorname",
+        id: "firstName",
+      }),
+      COLUMN_HELPER.accessor("lastName", {
+        header: "Nachname",
+        id: "lastName",
+      }),
+      COLUMN_HELPER.accessor("email", {
+        header: "E-Mail",
+        id: "email",
+        cell: MailCell,
+      }),
+      COLUMN_HELPER.accessor("phone", {
+        header: "Phone",
+        id: "phone",
+        cell: PhoneCell,
+      }),
+      COLUMN_HELPER.accessor((person) => person.peopleToRoles.map((p) => p.roles.name), {
+        header: "Rollen",
+        id: "roles",
+        cell: RolesCell,
+      }),
+    ],
+    getRowId: (person) => person.id,
+    getCoreRowModel: getCoreRowModel(),
+  };
+
+  return <Table options={options} />;
+}
--- app/(cms)/cms/people/page.tsx
+++ app/(cms)/cms/people/page.tsx
@@ -1,9 +1,29 @@
-import { Card, CardTitle } from "#/components/cms/card/Card";
+import { Card, CardContent, CardTitle } from "#/components/cms/card/Card";
+import { drizzle } from "#/lib/db/drizzle";
+import { PersonWithRoles } from "#/lib/types/people";
+import PeopleTable from "#/app/(cms)/cms/people/PeopleTable";
 
-export default function People() {
+async function getData(): Promise<PersonWithRoles[]> {
+  return await drizzle.query.people.findMany({
+    with: {
+      peopleToRoles: {
+        with: {
+          roles: true,
+        },
+      },
+    },
+  });
+}
+
+export default async function People() {
+  const data = await getData();
+
   return (
     <Card>
       <CardTitle>Personen</CardTitle>
+      <CardContent>
+        <PeopleTable data={data} />
+      </CardContent>
     </Card>
   );
 }
