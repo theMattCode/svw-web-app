191b617c9c34daead4a792e8ccd86aa3b75a56ce
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2025-06-16 16:30:49 +0200

    Hero activity carousel

---
+++ components/web/carousel/Carousel.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Children, PropsWithChildren, useCallback, useRef } from "react";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+const ITEM_WIDTH = 300;
+const ITEM_GAP = 16;
+const SCROLL_AMOUNT = ITEM_WIDTH + ITEM_GAP;
+
+export function Carousel({ children }: PropsWithChildren) {
+  const carouselRef = useRef<HTMLDivElement>(null);
+
+  const scrollByAmount = useCallback((direction: "left" | "right") => {
+    if (carouselRef.current) {
+      if (direction === "left") {
+        carouselRef.current.scrollBy({ left: -SCROLL_AMOUNT, behavior: "smooth" });
+      } else {
+        carouselRef.current.scrollBy({ left: SCROLL_AMOUNT, behavior: "smooth" });
+      }
+    }
+  }, []);
+
+  return (
+    <div className="flex flex-row items-center">
+      <div className="flex flex-row text-xl p-2 hover:bg-svw-blue-dark" onClick={() => scrollByAmount("left")}>
+        <FiChevronLeft />
+      </div>
+      <div className="relative w-full overflow-hidden">
+        <div className="flex gap-4 overflow-x-scroll no-scrollbar snap-x snap-mandatory" ref={carouselRef}>
+          {Children.map(children, (child) => (
+            <div className="flex-none w-32 @xl:w-48 snap-start">{child}</div>
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-row text-xl p-2 hover:bg-svw-blue-dark" onClick={() => scrollByAmount("right")}>
+        <FiChevronRight />
+      </div>
+    </div>
+  );
+}
